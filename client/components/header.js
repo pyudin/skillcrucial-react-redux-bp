1e68a7d2856f30735c237df7d43234980b7e9856
Pavel Yudin <pavel.yudin@sysgears.com>
2021-03-22 10:27:14 +0200

    site update

--- client/components/header.js
+++ client/components/header.js
@@ -26,7 +26,7 @@ const Header = () => {
       </div>
       {toggled && (
         <div id="menu" className="block">
-          <a href="www.google.com">
+          <a href="https://www.google.com/">
             <button
               type="button"
               className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white"
